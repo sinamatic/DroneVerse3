22ed8f1f99ec2fb0b7b46d4f559f5127138cfd7c
sinamatic <84576780+sinamatic@users.noreply.github.com>
2024-04-25 11:29:53 +0200

    oben, unten, rechts, links klappt, ausgabe richtung und x, y parameter, drohnensteuerung noch testen

--- main.py
+++ main.py
@@ -1,39 +1,39 @@
-# Region oben und unten: je 1/4 der Höhe
-# Zeigefinger in Region oben oder unten: oben/unten
-# Zeigefinger in Mitte: unterscheidung zwischen rechts und links
-# toDo: In Region Mitte Faust für "zu mir: vor" und offene Hand für "von mir weg: zurück"
-
 import cv2
 import mediapipe as mp
 from djitellopy import Tello
-from time import sleep
 
 # Initialisiere Mediapipe
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1)
 
 
-# Hauptfunktion zum Abrufen des Webcambildes und zur Erkennung der Handposition
-def main():
-
+# Funktion zur Initialisierung der Drohne
+def initialize_drone():
     drone = Tello()
-
     print("Drone initialized.")
     drone.connect()
     print("Drone connected.")
     drone.streamoff()
     drone.streamon()
-    # drone.streamoff()
+    return drone
+
+
+# Hauptfunktion zum Abrufen des Webcambildes und zur Erkennung der Handposition
+def main():
+    # drone = initialize_drone()
 
     cap = cv2.VideoCapture(0)  # Öffne die Kamera
     _, frame = cap.read()  # Lese ein Frame von der Kamera
-    height, width, _ = frame.shape
-    roi_top = int(height / 4)
-    roi_bottom = int(3 * height / 4)
-    roi_middle_left = int(width / 4)
-    roi_middle_right = int(3 * width / 4)
 
-    # drone.takeoff()
+    # define rois
+    height, width, roi_top, roi_bottom, roi_middle_left, roi_middle_right = define_rois(
+        frame
+    )
+
+    # reset drone position
+    x_direction = 0
+    y_direction = 0
+    z_direction = 0
 
     while True:
         ret, frame = cap.read()  # Lese ein Frame von der Kamera
@@ -45,6 +45,11 @@ def main():
         frame = cv2.flip(frame, 1)  # Horizontal spiegeln
         results = hands.process(frame_rgb)
 
+        # Zeichne die Regionen ein
+        draw_rois(
+            frame, height, width, roi_top, roi_bottom, roi_middle_left, roi_middle_right
+        )
+
         # Überprüfe, ob der Zeigefinger in der oberen, unteren oder mittleren Region liegt
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
@@ -52,40 +57,86 @@ def main():
                     mp_hands.HandLandmark.INDEX_FINGER_TIP
                 ]
                 finger_y = int(index_finger_tip.y * height)
-                finger_x = int(index_finger_tip.x * width)
+                # finger_x = int(index_finger_tip.x * width)
 
+                # Finger oben
                 if finger_y < roi_top:
-                    print("Oben")
+                    # y_direction = y_direction + 1
+                    y_direction = 1
+                    print("Up \t Y-Direction: {}".format(y_direction))
+
+                # Finger unten
                 elif finger_y > roi_bottom:
-                    print("Unten")
+                    y_direction = -1
+                    print("Down \t Y-Direction: {}".format(y_direction))
+
                 else:
                     thumb_tip = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
+
+                    # Finger rechts
                     if index_finger_tip.x < thumb_tip.x:
-                        print("Rechts")
+                        x_direction = 1
+                        print("Right \t X-Direction: {}".format(x_direction))
+
+                    # Finger links
                     elif index_finger_tip.x > thumb_tip.x:
-                        print("Links")
+                        x_direction = -1
+                        print("Left \t X-Direction: {}".format(x_direction))
+
+                    # Kein Finger
                     else:
                         print("Hand ist nicht ausgerichtet")
 
-        # Zeichne die Regionen ein
-        cv2.rectangle(
-            frame,
-            (roi_middle_left, roi_top),
-            (roi_middle_right, roi_bottom),
-            (0, 255, 0),
-            2,
-        )
-        cv2.rectangle(frame, (0, 0), (width, roi_top), (255, 0, 0), 2)
-        cv2.rectangle(frame, (0, roi_bottom), (width, height), (255, 0, 0), 2)
+                # Drohnensteuerung
+                # drone_control(drone, x_direction, y_direction)
 
         cv2.imshow("Frame", frame)  # Zeige das Frame mit OpenCV an
-        if cv2.waitKey(1) & 0xFF == ord(
-            "q"
-        ):  # Beende die Schleife, wenn 'q' gedrückt wird
+        key = cv2.waitKey(1)  # Warte auf eine Tastatureingabe (1 ms Timeout)
+        if key & 0xFF == ord("q"):  # Beende die Schleife, wenn 'q' gedrückt wird
             break
+        elif key & 0xFF == ord("s"):  # Wenn 's' gedrückt wird, gib "s gedrückt" aus
+            print("s gedrückt")
 
     cap.release()  # Gib die Ressourcen frei
     cv2.destroyAllWindows()
+
+
+def drone_control(drone, x_direction, y_direction):
+    if y_direction > 0:
+        drone.move_up(1)
+        print("Up")
+    elif y_direction < 0:
+        drone.move_down(1)
+        print("Down")
+    elif x_direction > 0:
+        drone.move_right(1)
+        print("Right")
+    elif x_direction < 0:
+        drone.move_left(1)
+        print("Left")
+
+
+def draw_rois(
+    frame, height, width, roi_top, roi_bottom, roi_middle_left, roi_middle_right
+):
+    cv2.rectangle(
+        frame,
+        (roi_middle_left, roi_top),
+        (roi_middle_right, roi_bottom),
+        (0, 255, 0),
+        2,
+    )
+    cv2.rectangle(frame, (0, 0), (width, roi_top), (255, 0, 0), 2)
+    cv2.rectangle(frame, (0, roi_bottom), (width, height), (255, 0, 0), 2)
+
+
+def define_rois(frame):
+    height, width, _ = frame.shape
+    roi_top = int(height / 4)
+    roi_bottom = int(3 * height / 4)
+    roi_middle_left = int(width / 4)
+    roi_middle_right = int(3 * width / 4)
+    return height, width, roi_top, roi_bottom, roi_middle_left, roi_middle_right
 
 
 if __name__ == "__main__":
