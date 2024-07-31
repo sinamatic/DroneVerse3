9ff8690610f3a45e846f1798086af9d9e91f386f
sinamatic <84576780+sinamatic@users.noreply.github.com>
2024-07-31 13:04:32 +0200

    ip deleted

--- tello_dronecontrol.py
+++ tello_dronecontrol.py
@@ -5,14 +5,15 @@
 This program provides a simple Tello drone controller for demonstration purposes.
 Needs to be updated, not tested yet.
 """
-from djitellopy import Tello, tello
+from djitellopy import Tello
 import time
 
 
 class TelloDroneController:
 
     def __init__(self):
-        self.drone = tello.Tello("192.168.50.101")
+        self.drone = Tello()
+        # self.drone2 = tello.Tello("192.168.50.102")
         print("Drone initialized.")
 
         # Initialisiere Geschwindigkeiten
@@ -26,6 +27,11 @@ class TelloDroneController:
         self.drone.streamoff()
         self.drone.streamon()
 
+        # self.drone2.connect()
+        # print("Drone2 connected.")
+        # self.drone2.streamoff()
+        # self.drone2.streamon()
+
         self.takeoff()  # Drohne hebt ab, sobald die Instanz erstellt wird
 
         self.speed = 15  # Geschwindigkeit der Drohne
@@ -37,6 +43,13 @@ class TelloDroneController:
             self.speed_up_down,
             self.yaw_speed,
         )
+
+        # self.drone2.send_rc_control(
+        #     self.speed_left_right,
+        #     self.speed_forward_back,
+        #     self.speed_up_down,
+        #     self.yaw_speed,
+        # )
 
     def up(self):
         self.speed_up_down = self.speed
@@ -70,6 +83,7 @@ class TelloDroneController:
 
     def takeoff(self):
         self.drone.takeoff()
+        # self.drone2.takeoff()
         self.update_movement()
         print("Tello: Takeoff")
 
@@ -85,3 +99,5 @@ class TelloDroneController:
     def land(self):
         self.drone.land()
         print("Tello: land")
+        # self.drone2.land()
+        # print("Tello2: land")
